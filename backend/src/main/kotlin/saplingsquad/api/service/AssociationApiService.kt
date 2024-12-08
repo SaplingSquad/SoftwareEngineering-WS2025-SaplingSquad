dcb0f38cc8c69f4531caa2c11f4e9ee0b68a53e1
hannah <hannah.coenen@uni-a.de>
2024-12-08 14:15:44 +0100

    feat(api): add api for association and project management
    This includes create, update and delete for both.

---
+++ backend/src/main/kotlin/saplingsquad/api/service/AssociationApiService.kt
@@ -0,0 +1,42 @@
+package saplingsquad.api.service
+
+import org.springframework.http.ResponseEntity
+import org.springframework.stereotype.Service
+import saplingsquad.api.AssociationsApiDelegate
+import saplingsquad.api.models.Association
+import saplingsquad.api.models.AssociationInformations
+import saplingsquad.api.models.Project
+import saplingsquad.api.models.ProjectInformations
+
+@Service
+class AssociationApiService() : AssociationsApiDelegate  {
+    override suspend fun createAssociation(association: Association?): ResponseEntity<Unit> {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun createProject(project: Project?): ResponseEntity<Unit> {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun deleteAssociation(userToken: String, assocId: Int): ResponseEntity<Unit> {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun deleteProject(userToken: String, projectId: Int): ResponseEntity<Unit> {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun updateAssociation(
+        userToken: String,
+        associationInformations: AssociationInformations?
+    ): ResponseEntity<Unit> {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun updateProject(
+        userToken: String,
+        projectInformations: ProjectInformations?
+    ): ResponseEntity<Unit> {
+        TODO("Not yet implemented")
+    }
+}
