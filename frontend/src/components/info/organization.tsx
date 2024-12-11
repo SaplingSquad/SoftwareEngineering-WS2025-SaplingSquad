7b81e4726bfd5397dd1cdb05230e241696acb751
FloezeTv <floeze@floeze.tv>
2024-12-11 19:33:55 +0100

    feat(frontend/map): map view
    Created map view which displays the markers from the backend.
    The markers are clustered when necessary.
    Some preliminary marker- and cluster-icons are included.
    The data is currently randomly generated.
    The popup-infos are currently implemented as stubs.
    The index-route will now display the map.
    Changed the body to be at least as high as the screen.

---
+++ frontend/src/components/info/organization.tsx
@@ -0,0 +1,8 @@
+import { component$ } from "@builder.io/qwik";
+
+/**
+ * Show information about a organization
+ */
+export const OrganizationInfo = component$(() => {
+  return <div>Organization Info</div>;
+});
