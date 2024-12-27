046f82d5d56953da156b1106c18440ec0f958c3e
Fabian Nowak <58370948+FabianNowak@users.noreply.github.com>
2024-12-27 15:58:15 +0100

    feat(backend): Codegen: access request object from all routes
    Workaround to get access to Authorization header later on.
    Maybe find a better solution.

--- backend/src/main/kotlin/saplingsquad/api/service/BookmarksApiService.kt
+++ backend/src/main/kotlin/saplingsquad/api/service/BookmarksApiService.kt
@@ -1,24 +1,28 @@
 package saplingsquad.api.service
 
 import org.springframework.http.ResponseEntity
+import org.springframework.http.server.reactive.ServerHttpRequest
 import org.springframework.stereotype.Service
+import org.springframework.web.context.request.NativeWebRequest
 import saplingsquad.api.BookmarksApiDelegate
+import java.util.*
 
 @Service
 class BookmarksApiService() : BookmarksApiDelegate  {
-    override suspend fun bookmarkOrganization(orgaId: Int): ResponseEntity<Unit> {
+
+    override suspend fun bookmarkOrganization(rawRequest: ServerHttpRequest, orgaId: Int): ResponseEntity<Unit> {
         TODO("Not yet implemented")
     }
 
-    override suspend fun bookmarkProject(projectId: Int): ResponseEntity<Unit> {
+    override suspend fun bookmarkProject(rawRequest: ServerHttpRequest, projectId: Int): ResponseEntity<Unit> {
         TODO("Not yet implemented")
     }
 
-    override suspend fun deleteOrganizationBookmark(orgaId: Int): ResponseEntity<Unit> {
+    override suspend fun deleteOrganizationBookmark(rawRequest: ServerHttpRequest, orgaId: Int): ResponseEntity<Unit> {
         TODO("Not yet implemented")
     }
 
-    override suspend fun deleteProjectBookmark(projectId: Int): ResponseEntity<Unit> {
+    override suspend fun deleteProjectBookmark(rawRequest: ServerHttpRequest, projectId: Int): ResponseEntity<Unit> {
         TODO("Not yet implemented")
     }
 }
--- backend/src/main/kotlin/saplingsquad/api/service/OrganizationApiService.kt
+++ backend/src/main/kotlin/saplingsquad/api/service/OrganizationApiService.kt
@@ -2,37 +2,47 @@ package saplingsquad.api.service
 
 import kotlinx.coroutines.flow.Flow
 import org.springframework.http.ResponseEntity
+import org.springframework.http.server.reactive.ServerHttpRequest
 import org.springframework.stereotype.Service
 import saplingsquad.api.OrganizationApiDelegate
 import saplingsquad.api.models.*
 
 @Service
 class OrganizationApiService() : OrganizationApiDelegate {
-    override suspend fun registerOrganization(organization: Organization): ResponseEntity<Int> {
+    override suspend fun registerOrganization(
+        rawRequest: ServerHttpRequest,
+        organization: Organization
+    ): ResponseEntity<Int> {
         TODO("Not yet implemented")
     }
 
-    override suspend fun getOrganization(): ResponseEntity<Organization> {
+    override suspend fun getOrganization(rawRequest: ServerHttpRequest): ResponseEntity<Organization> {
         TODO("Not yet implemented")
     }
 
-    override suspend fun updateOrganization(organizationDescriptions: OrganizationDescriptions?): ResponseEntity<Unit> {
+    override suspend fun updateOrganization(
+        rawRequest: ServerHttpRequest,
+        organizationDescriptions: OrganizationDescriptions?
+    ): ResponseEntity<Unit> {
         TODO("Not yet implemented")
     }
 
-    override suspend fun createProject(project: Project?): ResponseEntity<Int> {
+    override suspend fun createProject(rawRequest: ServerHttpRequest, project: Project?): ResponseEntity<Int> {
         TODO("Not yet implemented")
     }
 
-    override fun getProjectForOrga(): ResponseEntity<Flow<GetProjectForOrga200ResponseInner>> {
+    override fun getProjectForOrga(rawRequest: ServerHttpRequest): ResponseEntity<Flow<GetProjectForOrga200ResponseInner>> {
         TODO("Not yet implemented")
     }
 
-    override suspend fun updateProject(projectDescriptions: ProjectDescriptions?): ResponseEntity<Unit> {
+    override suspend fun updateProject(
+        rawRequest: ServerHttpRequest,
+        projectDescriptions: ProjectDescriptions?
+    ): ResponseEntity<Unit> {
         TODO("Not yet implemented")
     }
 
-    override suspend fun deleteProject(projectId: Int): ResponseEntity<Unit> {
+    override suspend fun deleteProject(rawRequest: ServerHttpRequest, projectId: Int): ResponseEntity<Unit> {
         TODO("Not yet implemented")
     }
 }
