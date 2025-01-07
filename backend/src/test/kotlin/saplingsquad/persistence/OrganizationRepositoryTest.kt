56fe1da054d8de7595b43cb5ac00d4ed9f688661
Fabian Nowak <58370948+FabianNowak@users.noreply.github.com>
2025-01-07 02:55:55 +0100

    test(backend): Test registerOrganization

--- backend/src/test/kotlin/saplingsquad/persistence/OrganizationRepositoryTest.kt
+++ backend/src/test/kotlin/saplingsquad/persistence/OrganizationRepositoryTest.kt
@@ -3,13 +3,22 @@ package saplingsquad.persistence
 import kotlinx.coroutines.flow.toList
 import kotlinx.coroutines.test.runTest
 import org.junit.jupiter.api.extension.ExtendWith
+import org.komapper.core.dsl.Meta
+import org.komapper.core.dsl.QueryDsl
+import org.komapper.core.dsl.query.single
+import org.komapper.r2dbc.R2dbcDatabase
+import org.komapper.tx.core.TransactionProperty
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.test.context.junit.jupiter.SpringExtension
+import saplingsquad.persistence.tables.CoordinatesEmbedded
+import saplingsquad.persistence.tables.OrganizationEntity
+import saplingsquad.persistence.tables.organizationEntity
 import saplingsquad.persistence.testconfig.ExampleOrgas
 import saplingsquad.persistence.testconfig.PersistenceTestConfiguration
 import kotlin.test.Test
 import kotlin.test.assertContains
 import kotlin.test.assertEquals
+import kotlin.test.assertIs
 
 /**
  * Test correct behavior of [OrganizationsRepository]
@@ -21,6 +30,9 @@ class OrganizationRepositoryTest {
     /** SUT */
     @Autowired
     lateinit var repository: OrganizationsRepository
+
+    @Autowired
+    lateinit var db: R2dbcDatabase
 
 
     /**
@@ -59,4 +71,36 @@ class OrganizationRepositoryTest {
         }
     }
 
+    /**
+     * Ensure that the organization entity is correctly inserted
+     * - all attributes are in DB
+     * - orga id was auto-generated
+     */
+    @Test
+    fun registerOrganizationInsertsAllColumns() = runTest {
+        val testOrg = OrganizationEntity(
+            orgId = 100, // must be ignored
+            name = "test-orga",
+            description = "test-description",
+            foundingYear = 2000,
+            memberCount = 100,
+            websiteUrl = "test-website-url",
+            donationUrl = "test-donation-url",
+            coordinates = CoordinatesEmbedded(50.0, 100.0)
+        )
+        val accountId = "account-1"
+        val inDb = db.withTransaction(transactionProperty = TransactionProperty.IsolationLevel.SERIALIZABLE) { tx ->
+            val result = repository.tryRegisterOrganization(accountId, testOrg)
+            val id = assertIs<OrganizationRegisterResult.Success>(result).id
+            val inDb = db.runQuery {
+                QueryDsl.from(Meta.organizationEntity).where {
+                    Meta.organizationEntity.orgId eq id
+                }.single()
+            }
+            tx.setRollbackOnly() //Rollback this transaction (only used for this test)
+            inDb
+        }
+        assert(inDb.orgId >= 1000) // Used auto generated id
+        assertEquals(testOrg, inDb.copy(orgId = 100))//reset orgId for comparison
+    }
 }
