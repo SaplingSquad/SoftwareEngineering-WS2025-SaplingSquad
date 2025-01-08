94075abd4adbe729bb8a2985fee32c77180fc14d
hannah <hannah.coenen@uni-a.de>
2025-01-08 15:43:19 +0100

    fix(backend): use korrekt key for bookmarks
    The previous setup allowed exactly one bookmark per user. Now one entry
    per (userId, projectId/orgaId) is possible.

--- backend/src/main/resources/db/changelog/0017-create-bookmark-tables.sql
+++ backend/src/main/resources/db/changelog/0017-create-bookmark-tables.sql
@@ -3,13 +3,15 @@
 --changeset 0017:1
 create table project_bookmarks
 (
-    account_id text primary key, /* OpenId Connect subject */
-    project_id integer references project unique
+    account_id text not null, /* OpenId Connect subject */
+    project_id integer references project unique,
+    primary key (account_id, project_id)
 );
 
 --changeset 0017:2
 create table organization_bookmarks
 (
-    account_id text primary key, /* OpenId Connect subject */
-    org_id integer references organization unique
+    account_id text not null, /* OpenId Connect subject */
+    org_id integer references organization unique,
+    primary key (account_id, org_id)
 );
