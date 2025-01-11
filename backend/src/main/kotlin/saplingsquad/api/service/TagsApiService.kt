cf1f114f2b6b6f052d48f9a7507008fe8a810e80
Hannah Coenen <124036232+hannahcoe@users.noreply.github.com>
2025-01-11 09:12:09 +0100

    fix(backend): add missing @Service annotation
    Co-authored-by: FabianNowak <58370948+FabianNowak@users.noreply.github.com>

--- backend/src/main/kotlin/saplingsquad/api/service/TagsApiService.kt
+++ backend/src/main/kotlin/saplingsquad/api/service/TagsApiService.kt
@@ -5,6 +5,7 @@ import org.springframework.http.ResponseEntity
 import saplingsquad.api.TagsApiDelegate
 import saplingsquad.api.models.GetTags200ResponseInner
 
+@Service
 class TagsApiService : TagsApiDelegate {
     override fun getTags(): ResponseEntity<Flow<GetTags200ResponseInner>> {
         TODO("Not yet implemented")
