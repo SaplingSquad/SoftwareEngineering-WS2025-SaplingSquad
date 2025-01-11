f13ceaa837a09907a22f362a2e5c71d8dde4374b
hannah <hannah.coenen@uni-a.de>
2025-01-11 09:30:40 +0100

    fix(backend): add missing import for @Service annotation in cf1f114

--- backend/src/main/kotlin/saplingsquad/api/service/TagsApiService.kt
+++ backend/src/main/kotlin/saplingsquad/api/service/TagsApiService.kt
@@ -2,6 +2,7 @@ package saplingsquad.api.service
 
 import kotlinx.coroutines.flow.Flow
 import org.springframework.http.ResponseEntity
+import org.springframework.stereotype.Service
 import saplingsquad.api.TagsApiDelegate
 import saplingsquad.api.models.GetTags200ResponseInner
 
