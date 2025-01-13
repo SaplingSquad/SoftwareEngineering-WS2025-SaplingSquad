81d0137711bcac67037bbafc093235bca0c7c2b0
Fabian Nowak <58370948+FabianNowak@users.noreply.github.com>
2025-01-13 12:17:56 +0100

    style(backend): Use typealias for Ids

--- backend/src/main/kotlin/saplingsquad/persistence/tables/Region.kt
+++ backend/src/main/kotlin/saplingsquad/persistence/tables/Region.kt
@@ -5,13 +5,16 @@ import org.komapper.annotation.KomapperEntity
 import org.komapper.annotation.KomapperId
 import org.komapper.annotation.KomapperTable
 
+typealias RegionId = String
+typealias ContinentId = String
+
 @KomapperTable("regions")
 @KomapperEntity
 data class RegionEntity(
     @KomapperId
-    val regionId: String,
+    val regionId: RegionId,
     val name: String,
-    val continentId: String,
+    val continentId: ContinentId,
     val continent: String,
 )
 
@@ -19,6 +22,6 @@ data class RegionEntity(
 @KomapperEntity
 data class ContinentEntity(
     @KomapperId
-    val continentId: String,
+    val continentId: ContinentId,
     val continent: String,
 )
