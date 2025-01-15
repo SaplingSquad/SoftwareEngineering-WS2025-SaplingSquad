094685d70789b49064e220234e60d0fbc6a049da
FloezeTv <floeze@floeze.tv>
2025-01-15 15:06:42 +0100

    feat(frontend/info): set generic titles for now
    Could later change this to display the name of the project/organization.

--- frontend/src/routes/project/[id]/index.tsx
+++ frontend/src/routes/project/[id]/index.tsx
@@ -1,7 +1,12 @@
 import { component$ } from "@builder.io/qwik";
+import { DocumentHead } from "@builder.io/qwik-city";
 import { ProjectInfo } from "~/components/info/project";
 import { InfoPage } from "~/views/info_page";
 
 export default component$(() => {
   return <InfoPage InfoComponent={ProjectInfo} />;
 });
+
+export const head: DocumentHead = {
+  title: "Sprout - Projekt",
+};
