0648feffd3535a5116f07b576deda2068cb33207
Maximilian Mitterrutzner <maximitterrutzner@gmail.com>
2025-01-15 23:10:20 +0100

    fix(frontend): fix not being able to choose preferences

--- frontend/src/routes/questions/index.tsx
+++ frontend/src/routes/questions/index.tsx
@@ -1,20 +1,23 @@
 import {
   $,
   component$,
-  Resource,
   useOnWindow,
   useSignal,
+  useStore,
+  useTask$,
 } from "@builder.io/qwik";
-import { useGetQuestions } from "~/api/api_hooks.gen";
-import { ApiResponse } from "~/components/api";
-import { QuestionCard, QuestionCardProps } from "~/components/question-card";
+import { getQuestions } from "~/api/api_methods.gen";
+import { ApiUnreachable } from "~/components/api";
+import {
+  type QuestionCardProps,
+  QuestionCard,
+} from "~/components/question-card";
 
-type Question = {
-  id: number | undefined;
-  title: string | undefined;
-  text: string | undefined;
-  imageUrl: string | undefined;
-};
+enum State {
+  LOADING,
+  ERROR,
+  SUCCESS,
+}
 
 /**
  * Shows the main question window, displaying the questions in a grid.
@@ -22,9 +25,27 @@ type Question = {
 export default component$(() => {
   const buttonRef = useSignal<HTMLButtonElement>();
   const buttonWidth = useSignal<number | null>(null);
+  const state = useSignal<State>(State.LOADING);
+  const questionCardStore = useStore<{ props: QuestionCardProps[] }>({
+    props: [],
+  });
 
-  const request = useGetQuestions();
-  const questionCardProps = useSignal<QuestionCardProps[]>([]);
+  useTask$(async () => {
+    await getQuestions().then(
+      (r) => {
+        if (r.status === 200) {
+          questionCardStore.props = r.body.map((q) => ({
+            isSelected: false,
+            ...q,
+          }));
+          state.value = State.SUCCESS;
+        } else {
+          state.value = State.ERROR;
+        }
+      },
+      () => (state.value = State.ERROR),
+    );
+  });
 
   const updateButtonWidth = $(() => {
     if (buttonRef.value) {
@@ -35,56 +56,42 @@ export default component$(() => {
   useOnWindow("load", updateButtonWidth);
   useOnWindow("resize", updateButtonWidth);
 
-  return (
-    <Resource
-      value={request}
-      onResolved={(response) => (
-        <ApiResponse
-          response={response}
-          on200$={(data: Question[]) => {
-            questionCardProps.value = data.map((q) => ({
-              isSelected: false,
-              ...q,
-            }));
-
-            return (
-              <div class="min-h-screen bg-base-100 py-10 pb-28">
-                <h1 class="mb-2 text-center text-4xl font-bold">
-                  Worauf möchtest du deinen Fokus legen?
-                </h1>
-                <h2 class="mb-8 text-center text-xl font-bold">
-                  Klicke Themen die deinen Interessen entsprechen einfach an
-                </h2>
-                <div class="flex flex-wrap justify-around">
-                  {questionCardProps.value.map((item, idx: number) => (
-                    <QuestionCard key={idx} data={item} />
-                  ))}
-                </div>
-                <a
-                  ref={buttonRef}
-                  class="btn btn-lg fixed bottom-6 right-10 border border-4 border-primary bg-primary-content text-primary hover:scale-[1.02] hover:border-secondary hover:bg-base-100"
-                  href="/map"
-                  style={{
-                    width: buttonWidth.value
-                      ? buttonWidth.value + 1 + "px"
-                      : "auto",
-                  }}
-                >
-                  {questionCardProps.value.every((q) => !q.isSelected)
-                    ? "Überspringen 🡒"
-                    : "Weiter 🡒"}
-                </a>
-              </div>
-            );
-          }}
-          defaultError$={(code) => <p>{code}</p>}
-        />
-      )}
-      onRejected={() => (
-        <p class="text-xl font-bold text-error">
-          Verbindung fehlgeschlagen, bitte lade die Seite neu!
-        </p>
-      )}
-    />
-  );
+  switch (state.value) {
+    case State.LOADING:
+      return (
+        <div class="align-center flex h-screen w-screen items-center bg-base-100">
+          <span class="loading loading-dots loading-xs text-primary"></span>
+        </div>
+      );
+    case State.ERROR:
+      return <ApiUnreachable error={Error("Questions fetch failed")} />;
+    case State.SUCCESS:
+      return (
+        <div class="min-h-screen bg-base-100 py-10 pb-28">
+          <h1 class="mb-2 text-center text-4xl font-bold">
+            Worauf möchtest du deinen Fokus legen?
+          </h1>
+          <h2 class="mb-8 text-center text-xl font-bold">
+            Klicke Themen die deinen Interessen entsprechen einfach an
+          </h2>
+          <div class="flex flex-wrap justify-around">
+            {questionCardStore.props.map((item, idx: number) => (
+              <QuestionCard key={idx} data={item} />
+            ))}
+          </div>
+          <a
+            ref={buttonRef}
+            class="btn btn-lg fixed bottom-6 right-10 border border-4 border-primary bg-primary-content text-primary hover:scale-[1.02] hover:border-secondary hover:bg-base-100"
+            href="/map"
+            style={{
+              width: buttonWidth.value ? buttonWidth.value + 1 + "px" : "auto",
+            }}
+          >
+            {questionCardStore.props.every((q) => !q.isSelected)
+              ? "Überspringen 🡒"
+              : "Weiter 🡒"}
+          </a>
+        </div>
+      );
+  }
 });
